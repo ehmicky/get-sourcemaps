d779a378c9c7840ac5c8056a365c39032beba8b8
ehmicky <ehmicky@gmail.com>
2019-05-27 10:00:00 +0200

    Use test-each

--- test/main.js
+++ test/main.js
@@ -1,25 +1,80 @@
+import { Buffer } from 'buffer'
+
 import test from 'ava'
+import testEach from 'test-each'
 
 import { parse, test as testComment } from '../src/main.js'
 
-import { COMMENTS } from './helpers/fixtures.js'
+testEach(
+  [
+    // Valid
+    '//# sourceMappingURL=file.js',
+    '/* # sourceMappingURL=file.js */',
+    '   //# sourceMappingURL=file.js',
+    '// #sourceMappingURL=file.js',
+    '/*# sourceMappingURL=file.js */',
+    '/* # sourceMappingURL=file.js*/',
+    '/*# sourceMappingURL=file.js*/',
+    '//@ sourceMappingURL=file.js',
+    '//#sourceMappingURL=file.js',
+    '//# sourceMappingUrl=file.js',
+    '//# sourceMappingURL = file.js',
+    '//# sourceMappingURL=/',
+    '//# sourceMappingURL=http://www.domain.com/path',
+    '//# sourceMappingURL=file.js   ',
+    '/* # sourceMappingURL=file.js */   ',
+    Buffer.from('//# sourceMappingURL=file.js '),
 
-COMMENTS.forEach(comment => {
-  test(`should test: ${comment}`, t => {
-    try {
-      const value = testComment(comment)
-      t.snapshot(value)
-    } catch (error) {
-      t.snapshot(error.message)
-    }
-  })
+    // Valid multiline
+    'Before\n//# sourceMappingURL=file.js',
+    'Before\r\n//# sourceMappingURL=file.js',
+    '//# sourceMappingURL=file.js\nAfter',
+    '//# sourceMappingURL=file.js\r\nAfter',
+    '//# sourceMappingURL=file.js\n//# sourceMappingURL=two.js',
 
-  test(`should parse: ${comment}`, t => {
-    try {
-      const value = parse(comment)
-      t.snapshot(value)
-    } catch (error) {
-      t.snapshot(error.message)
-    }
-  })
-})
+    // Valid data URI
+    '//# sourceMappingURL=data:application/json;charset=utf-8;base64,e30=',
+    '//# sourceMappingURL=data:;charset=utf-8;base64,e30=',
+    '//# sourceMappingURL=data:application/json;base64,e30=',
+
+    // Invalid URI
+    '//# sourceMappingURL=http://',
+
+    // Invalid charset/format
+    '//# sourceMappingURL=data:invalid_mime;charset=utf-8;base64,e30=',
+    '//# sourceMappingURL=data:charset=utf-8;base64,e30=',
+    '//# sourceMappingURL=data:application/json;charset=ascii;base64,e30=',
+
+    // Invalid base64
+    '//# sourceMappingURL=data:application/json;charset=utf-8;base64,invalid_base64',
+
+    // Invalid
+    true,
+    '///# sourceMappingURL=file.js',
+    '.  //# sourceMappingURL=file.js',
+    '//# sourceMappingURL',
+    '//# sourceMappingURL=',
+    '//# sourceMappingURL=http://www.domain.com/not percent encoded',
+    '/* # sourceMappingURL=file*/.js */',
+    '//# sourceMappingURL=file.js  .',
+  ],
+  ({ title }, comment) => {
+    test(`should test | ${title}`, t => {
+      try {
+        const value = testComment(comment)
+        t.snapshot(value)
+      } catch (error) {
+        t.snapshot(error.message)
+      }
+    })
+
+    test(`should parse | ${title}`, t => {
+      try {
+        const value = parse(comment)
+        t.snapshot(value)
+      } catch (error) {
+        t.snapshot(error.message)
+      }
+    })
+  },
+)
